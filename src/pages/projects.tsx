6d15fff511c24d848c8a9dbafab0267ad4c1b578
Dimitri Hector <joshuahector86@gmail.com>
2024-12-04 14:36:46 -0500

    adding the portolio captions

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,39 @@
 import Page from "@/components/layout/page";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { project_data } from "@/potfolio-data/project-data";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
     <Page>
-      <div></div>
+      <div className="w-full flex flex-col items-center">
+        {project_data.map((project) => (
+          <>
+            <Card className="mt-2 sm:max-w-[70%]">
+              <CardHeader className="flex-row  justify-between items-center">
+                <h3 className="font-semibold sm:text-xl">
+                  {project.projectName}
+                </h3>
+                <Link to={project.url} target="_blank">
+                  <Button variant={"secondary"}>Go to site</Button>
+                </Link>
+              </CardHeader>
+              <CardContent>
+                <img src={project.projectImg} alt="project-img" />
+              </CardContent>
+              <CardFooter>{project.projectCaption}</CardFooter>
+            </Card>
+            <Separator />
+          </>
+        ))}
+      </div>
     </Page>
   );
 };
--- src/potfolio-data/project-data.ts
+++ src/potfolio-data/project-data.ts
@@ -11,45 +11,50 @@ import {
 export const project_data = [
   {
     projectName: "Hectorgram",
-    url: "",
+    url: "hhttps://hectorgram.vercel.app/",
     projectImg: hectorgram,
     projectCaption:
-      "Full Stack instagram style clone that uses Appwrite backend as a service",
+      "Full Stack instagram clone that uses Appwrite backend as a service.",
   },
   {
     projectName: "Chat App",
-    url: "",
+    url: "https://chatapp-theta-rouge.vercel.app/",
     projectImg: chat_app,
-    projectCaption: "",
+    projectCaption:
+      "Chat application UI that sets up conversation support using Zustand as the global state management solution.",
   },
   {
     projectName: "CloneTube",
-    url: "",
+    url: "https://youtube-clone-phi-wine.vercel.app/",
     projectImg: clone_tube,
-    projectCaption: "",
+    projectCaption:
+      "UI Only youtube clone done with ShadCN, TailwindCss, and React Typescript.",
   },
   {
     projectName: "Everything Dash",
-    url: "",
+    url: "https://llm-based-corporate-dash-aolp.vercel.app/",
     projectImg: everything_dash,
-    projectCaption: "",
+    projectCaption:
+      "UI Only dashboard deskstop site done with ShadCN, Tailwind, and React Typescript.",
   },
   {
     projectName: "Personal Site",
-    url: "",
+    url: "https://dimitrihector.com/",
     projectImg: personal_site,
-    projectCaption: "",
+    projectCaption: "Personal website.",
   },
   {
     projectName: "Portfolio Site - (Old)",
-    url: "",
+    url: "https://joshuahector86.github.io/professional_site_2023/",
     projectImg: portfolio_site_2,
-    projectCaption: "",
+    projectCaption:
+      "An old portfolio site made with React Typescript, SCSS, and Bootstrap.",
   },
   {
     projectName: "Portfolio Site - (Old)",
-    url: "",
+    url: "https://joshuahector86.github.io/professional-site/",
     projectImg: portfolio_site_1,
-    projectCaption: "",
+    projectCaption:
+      "An old portfolio site made with React Typescript, SCSS, and Bootstrap.",
   },
 ];
